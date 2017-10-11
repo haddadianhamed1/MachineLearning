95d838baa0480736d3bf870a95f00286a1fbc0ae
Hamedh <hamedh@dnanexus.com>
2017-10-10 23:35:17 -0700

    first class

---
+++ ann.py
@@ -0,0 +1,23 @@
+class ANN:
+    # representing Artifical Neural Network
+    # will have couple of layers
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def update_weights(self):
+        pass
+
+
+
+
+class Layer:
+    # representing a single layer of Neurons
+    # collection of Neurons
+    def __init__(self):
+        pass
